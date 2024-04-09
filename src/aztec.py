4cd0b84a7ecfb26e91c288e7efbada043aeee3c7
Bareca <besteststurbina@gmail.com>
2024-04-09 13:44:41 +0100

    gl 2 primeiro e quarto caso

--- src/aztec.py
+++ src/aztec.py
@@ -1,9 +1,25 @@
 def aztec(G, c, r, rows, columns):
-    return "oi"
+    if columns == rows and c == r and c == 1:
+        return factorial(rows)
+    dp = [[0] * (columns + 1) for _ in range(rows + 1)]
+    dp[0][0] = 1
+
+    for i in range(1, rows + 1):
+        for j in range(1, columns + 1):
+            if i < r or j < c:
+                dp[i][j] = 0
+            else:
+                dp[i][j] = dp[i - 1][j - c] * binomial(j, c) * binomial(i - 1, r - 1)
+
+    return dp[rows][columns]
 
 
+def binomial(n: int, k: int) -> int:
+    return factorial(n) // (factorial(k) * factorial(n - k))
 
 
+def factorial(number: int) -> int:
+    return 1 if number == 0 or number == 1 else number * factorial(number - 1)
 
 
 def main():
@@ -15,7 +31,6 @@ def main():
 
         result = aztec(G, c, r, rows, columns)
         print(result)
-
 
 
 if __name__ == "__main__":
